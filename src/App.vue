803dd3a9a794711bbb2037ef9b5804b9aad47bc7
hasemmallicksmg <hasem.mallick@skymapglobal.com>
2024-01-11 14:47:47 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div style="font-size: larger; text-align: center;">
+    Vue Application
+  </div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
